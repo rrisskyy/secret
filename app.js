226753ec44cc324a0468235960585d2f7540022e
rrisskyy <riskykurniawan945@gmail.com>
2021-10-25 22:18:33 +0800

    Level 1

--- app.js
+++ app.js
@@ -2,11 +2,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const mongoose = require("mongoose");
 
-//jshint esversion:6
-// import express from "express";
-// import bodyParser from "body-parser";
-// import ejs from "ejs";
+
 
 const app = express();
 
@@ -14,18 +12,66 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 app.use(bodyParser.urlencoded({ extended: true }));
 
+mongoose.connect("mongodb://localhost:27017/userDB", {useNewUrlParser: true});
+
+const userSchema = {
+    email: String,
+    password: String
+};
+
+const User = new mongoose.model("User", userSchema);
+
+
 
 
 
 app.get("/", (req, res) => {
     res.render("home");
-})
-app.get("/register", (req, res) => {
-    res.render("login");
-})
-app.get("/login", (req, res) => {
-    res.render("login");
-})
+});
+
+app.route("/register")
+    .get((req, res) => {
+        res.render("register");
+    })
+    .post((req, res) => {
+        const newUser = new User({
+            email: req.body.username,
+            password: req.body.password
+        });
+        newUser.save((err) => {
+            if(err) {
+                console.log(err);
+            } else {
+                res.render("secrets");
+            }
+        })
+    });
+
+
+app.route("/login")    
+    .get((req, res) => {
+        res.render("login");
+    })
+
+    .post((req, res) => {
+        const username = req.body.username;
+        const password = req.body.password;
+
+        User.findOne({email: username}, (err, foundUser) => {
+            if(err) {
+                console.log(err);
+            } else {
+                if(foundUser) {
+                    if(foundUser.password === password) {
+                        res.render("secrets");
+                    }
+                }
+            }
+    
+
+        })
+    });
+
 
 
 app.listen(3000, () => {
